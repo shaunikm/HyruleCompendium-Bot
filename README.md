919427b555cf6e71e43a478625f6d3955af4a8bf
shaunik.musukula <shaunik.musukula@gmail.com>
2021-01-03 20:13:36 -0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+<div align="center">
+<img src="https://www.pngkey.com/png/full/17-179750_discord-icon-discord-logo.png" width="12%">
+<h3> Hyrule Compendium Discord Bot</h3>
+<p> A simple discord bot that uses the <a href="https://github.com/shaunikm/Hyrule-Compendium-python-client">Hyrule Compendium Python Library</a> to retrieve data about Hyrule!</p>
+<h3>Invite Link</h3>
+<p>You can invite the bot <a href="https://discord.com/oauth2/authorize?client_id=795444152443207680&permissions=509952&scope=bot">here</a>.</p>
+<h3>Extra Links</h3>
+<li><a href="https://github.com/gadhagod/Hyrule-Compendium-API">Hyrule Compendium API</a></li>
+<li><a href="https://www.pngkey.com/png/full/17-179750_discord-icon-discord-logo.png">Discord Image Credit</a></li>
+</div>
+<br>
+<br>
+<div align="right">
+<p>Made with awesomeness by <a href="https://github.com/shaunikm">@shaunikm</a></p>
+</div>
---
+++ main.py
@@ -0,0 +1,69 @@
+import os
+import discord
+from discord.ext import commands
+from HyruleCompendium import getEntry
+
+client = commands.Bot(command_prefix='!')
+
+client.remove_command('help')
+commandsHelpList = ['!help', '!invite', '!search']
+descriptionHelpList = ['View the message you\'re seeing right now.',
+                       'Get the invite link for this bot to invite it to more servers!',
+                       'Search for an entry in the Hyrule Compendium']
+
+
+def capAll(inStr: str):
+    termList = [i for i in inStr.split()]
+    for i in termList:
+        termList[termList.index(i)] = termList[termList.index(i)].capitalize()
+    return ' '.join(i for i in termList)
+
+
+@client.event
+async def on_ready():
+    print('Bot is ready!')
+
+
+@client.command(aliases=['inv', 'i'])
+async def invite(ctx):
+    embed = discord.Embed(colour=discord.Colour.light_gray())
+    embed.add_field(name='Invite Link',
+                    value='\
+                    [Click here](https://discord.com/api/oauth2/authorize?client_id=795444152443207680&permissions=509952&scope=bot)')
+    await ctx.send(embed=embed)
+
+
+@client.command(aliases=['h'])
+async def help(ctx):
+    embed = discord.Embed(colour=discord.Colour.red())
+    for command, description in zip(commandsHelpList, descriptionHelpList):
+        embed.add_field(name=command, value=description, inline=False)
+    await ctx.send(embed=embed)
+
+
+@client.command(aliases=['s'])
+async def search(ctx, *, term):
+    embed = discord.Embed(colour=discord.Colour.gold())
+    try:
+        data = getEntry(term.lower())
+    except ValueError:
+        await ctx.send('Invalid search term!')
+        return
+    if not data:
+        await ctx.send('Invalid search term!')
+        return
+    term = capAll(term)
+    embed.set_author(name=term)
+    embed.add_field(name='Category', value=capAll(data['category']), inline=False)
+    embed.add_field(name='ID', value=data['id'], inline=False)
+    embed.add_field(name='Description', value=data['description'], inline=False)
+    embed.add_field(name='Drops', value='\n'.join(' - ' + capAll(i) for i in data['drops']), inline=False)
+    await ctx.send(embed=embed)
+
+
+@client.command(aliases=['p'])
+async def ping(ctx):
+    await ctx.send(f'Pong!\nClient Latency: `{round(client.latency*1000)}ms`')
+
+
+client.run(os.getenv('token'))
